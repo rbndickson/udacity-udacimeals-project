6e6e51a9303b935d402b2b0296b5550751dba8b4
Robin Dickson <robindickson@gmail.com>
2017-08-08 21:52:18 +0200

    feat: Add redux tools

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import './index.css';
 import { createStore } from 'redux';
 import reducer from './reducers'
 
-const store = createStore(reducer);
-
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 ReactDOM.render(
-  <App />,
+  <App store={store}/>,
   document.getElementById('root')
 );
