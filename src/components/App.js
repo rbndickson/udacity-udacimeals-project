90aeab65705a62f5f7bc8d85c6904c290f16d7cb
Robin Dickson <robindickson@gmail.com>
2017-09-07 18:09:51 +0200

    feat: Add calendar grid

--- src/components/App.js
+++ src/components/App.js
@@ -19,6 +19,29 @@ class App extends Component {
             </li>
           ))}
         </ul>
+        <div className='calendar'>
+          <div className='days'>
+            {calendar.map(({ day }) => <h3 key={day} className='subheader'>{capitalize(day)}</h3>)}
+          </div>
+          <div className='icon-grid'>
+            {calendar.map(({ day, meals }) => (
+              <ul key={day}>
+                {mealOrder.map((meal) => (
+                  <li key={meal} className='meal'>
+                    {meals[meal]
+                      ? <div className='food-item'>
+                          <img src={meals[meal].image} alt={meals[meal].label}/>
+                          <button onClick={() => remove({meal, day})}>Clear</button>
+                        </div>
+                      : <button className='icon-btn'>
+                          <CalendarIcon size={30}/>
+                        </button>}
+                  </li>
+                ))}
+              </ul>
+            ))}
+          </div>
+        </div>
       </div>
     )
   }
